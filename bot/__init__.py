635a8ab7c4c79ea3616370606a919d62daf08370
aryan sharma <aryansharma6745@gmail.com>
2026-07-02 10:46:38 +0530

    Create __init__.py for trading bot package
    Initialize trading bot package with module exports.

---
+++ bot/__init__.py
@@ -0,0 +1,11 @@
+"""Trading bot package for Binance USDT-M Futures Testnet."""
+
+__all__ = [
+    "client",
+    "config",
+    "exceptions",
+    "logging_config",
+    "orders",
+    "utils",
+    "validators",
+]
